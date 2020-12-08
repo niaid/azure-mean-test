255f1177f3181e7ff5a1c3e33602961e87039a94
jake.jester@gmail.com <jake.jester@nih.com>
2020-12-08 09:41:51 -0500

    deployed with correct host header

---
+++ app/vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  devServer: {
+    public: 'mean-test.azurewebsites.net'
+  }
+};
--- deploy.sh
+++ deploy.sh
@@ -10,3 +10,5 @@ docker build -t jjestertestmean.azurecr.io/mean-test .
 docker push jjestertestmean.azurecr.io/mean-test:latest
 
 az acr repository list -n jjestertestmean
+
+az webapp restart --name mean-test --resource-group jjester-test-mean
