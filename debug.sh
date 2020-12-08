12f854ccd7775807ed17c2352a8bd078747a0b40
jake.jester@gmail.com <jake.jester@nih.com>
2020-12-08 08:57:24 -0500

    deploy to azure works

--- debug.sh
+++
@@ -1,3 +0,0 @@
-docker kill mean-test
-docker build -t mean-test .
-docker run -it -p 8080:8080 mean-test
---
+++ deploy.sh
@@ -0,0 +1,12 @@
+export AZ_ACR_USERNAME='jjestertestmean'
+echo $AZ_ACR_USERNAME
+
+export AZ_ACR_PASSWORD=$(az acr credential show --resource-group jjester-test-mean --name jjestertestmean | jq -r '.passwords[0].value')
+echo $AZ_ACR_PASSWORD
+
+docker login jjestertestmean.azurecr.io --username $AZ_ACR_USERNAME --password $AZ_ACR_PASSWORD
+docker build -t jjestertestmean.azurecr.io/mean-test .
+
+docker push jjestertestmean.azurecr.io/mean-test:latest
+
+az acr repository list -n jjestertestmean
